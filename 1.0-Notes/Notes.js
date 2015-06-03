62ce1e0e616679da0fa749191c4a73c3edb93056
sosana <thesosana@gmail.com>
2015-06-03 01:59:42 +0000

    updating notes

--- 1.0-Notes/Notes.js
+++ 1.0-Notes/Notes.js
@@ -1,4 +1,15 @@
 /*
- * Add Notes Here
+ * WHY REACT
+ *  - UI code is readable and maintainable
+ *  - Creating UI components allows for modularity and code reusability
+ *  - React can be rendered on both client & server side
+ *  - It uses a concept called Virtual DOM that selectively renders subtrees of nodes upon state changes.
+ * 
+ * ReactJS Virtual DOM
+ *  - React runs a diffing algorithm which at exactly has changed
+ *  - Reconcilition where it updates the document object model with the results of diff
+ *  - in other words it will only change the child attributes we updated while not affecting the parent 
+ *    or changing the other child elements
+ * 
  * 
 */
--- 1.0-Notes/componentNotes.js
+++ 1.0-Notes/componentNotes.js
@@ -1,4 +1,25 @@
 /*
- * Add Notes Here
+ * COMPONENT STATE & LIFE CYCLE
+ *  - components are classes in react js
+ * 
+ * 
+ *  - render
+ *  - getInitialState
+ *  - getDefaultProps
+ *  - propTypes
+ *  - mixins
+ *  - statics
+ *  - displayName
+ *  - States & Methods
+ *  - Mount
+ *  - componentWillMount
+ *  - componentDidMount
+ *  - Update
+ *  - componentWillReceiveProps
+ *  - shouldComponentUpdate
+ *  - componentWillUpdate
+ *  - componentDidUpdate
+ *  - Unmount
+ *  - componentWillUnmount
  * 
 */
