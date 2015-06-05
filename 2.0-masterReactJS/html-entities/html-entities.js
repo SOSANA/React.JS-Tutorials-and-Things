a141ecce77f428e310e9da30892c232367dcca04
sosana <thesosana@gmail.com>
2015-06-05 18:38:37 +0000

    adding properties to courses element for html entites

--- 2.0-masterReactJS/html-entities/html-entities.js
+++ 2.0-masterReactJS/html-entities/html-entities.js
@@ -1,2 +1,20 @@
 var element = <div>&copy; skillbakery.com</div>
-React.render(element,document.getElementById("divContainer"));
+//React.render(element,document.getElementById("divContainer"));
+
+var Courses = React.createClass({
+    render: function() {
+        return (
+            <p>
+            Course Name: <input type="text" placeholder="Course name here"/>
+            {this.props.copy} skillbaker.com
+            </p>            
+        );    
+    }
+});
+
+
+// the reason we pass in the properties with html entities for charcode is 
+// because react double scapped to prevent SSL attacks
+// making use of the html entity for copy right symbol with CharCode
+var courseElement = <Courses copy={String.fromCharCode(169)}/>;
+React.render(courseElement, document.getElementById("divContainer"));
