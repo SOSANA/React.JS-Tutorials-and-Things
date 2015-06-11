8b6775bd7b43fb3c3937cfc44ed72632860271b9
sosana <thesosana@gmail.com>
2015-06-11 01:54:48 +0000

    creating a new folder for creating forms and events in reactjs

---
+++ 2.0-masterReactJS/reviewForm/reviewForm.js
@@ -0,0 +1,53 @@
+/*
+ *  using multiple components with getInitialState and some the events that are associated with components
+ *  such as change events that we can associate with textboxes, drop downs and form submitting events
+ *  
+*/
+
+var courseList = [
+    {courseName: "Master ReactJS"},
+    {courseName: "Master AngularJS"},
+    {courseName: "Master Hacking"},
+    {courseName: "Master immutableJS"},
+    {courseName: "Master RadiumJS"},
+    {courseName: "Master NodeJS"},
+    {courseName: "Master ExpressJS"},
+    {courseName: "Master BaconJS"},
+    {courseName: "Master MongooseJS"}
+]
+var Courses = React.createClass({
+    render: function() {
+        return (
+            <div>
+                We have following courses to offer
+                <CourseCollection list = {this.props.list} />
+            </div>
+        );
+    }
+});
+
+var CourseCollection = React.createClass({
+    render: function() {
+        // this exposes a method map function that loops through our list, passing cours as arugment
+        var course = this.props.list.map(function(course){
+            // Course clase is being returned with course arugment and courseName which is our list
+            return <Course courseName = {course.courseName} />
+        });
+        return (
+            <div> {course} </div>
+        );
+    }
+});
+
+var Course = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.courseName}
+            </div>
+        );
+    }
+});
+
+var coursesElement = <Courses list={courseList} />;
+React.render(coursesElement,document.getElementById('divContainer'));
