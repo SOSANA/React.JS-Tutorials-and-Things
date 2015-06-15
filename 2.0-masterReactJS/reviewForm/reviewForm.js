2cce4fdebe9075ece075272e326aa8a0fef3adfa
sosana <thesosana@gmail.com>
2015-06-15 18:40:14 +0000

    updating form comments and form elements

--- 2.0-masterReactJS/reviewForm/reviewForm.js
+++ 2.0-masterReactJS/reviewForm/reviewForm.js
@@ -1,10 +1,12 @@
-/*
+/**
+ * 
  *  using multiple components with getInitialState and some the events that are associated with components
  *  such as change events that we can associate with textboxes, drop downs and form submitting events
- *  left off at 3:09
-*/
+ * 
+ */
 
 var courseList = [
+    {courseName: "Pick One"},
     {courseName: "Master ReactJS"},
     {courseName: "Master AngularJS"},
     {courseName: "Master Hacking"},
@@ -17,26 +19,36 @@ var courseList = [
 ]
 
 var ReviewControl = React.createClass({
-    
+    // preparing our initals values with empty fields, and reviews will be an empty array of collections
     getInitialState: function() {
         return {name: '', feedback: '', course: '', reviews: []};
     },
+    // these are the functions (onChangeName, onChangeFeedback, onChangeCourse) that we have associated with 
+    // our controls for input and select
+    // we are assigning what value is in the textbox to name state
     onChangeName: function(el) {
         this.setState({name: el.target.value})
     },
+    // we are assigning what value is in the textbox to feedback state
     onChangeFeedback: function(el) {
         this.setState({feedback: el.target.value})
     },
+    // we are assigning what value in the dropdown box to course state
     onChangeCourse: function(el) {
         this.setState({course: el.target.value})
     },
+    // once this is done the form this method gets called
     submitReview: function(el) {
+        // preventing form from getting submitted
         el.preventDefault();
+        // we are adding the feedback to the reviews collection
+        // this reviews collections is actually containing name, feedback, and course
         this.state.reviews.push({
             name: this.state.name, 
             feedback: this.state.feedback, 
             course: this.state.course
         });
+        // once that is done we are refreshing our text boxes name and feedback to blank
         this.setState({
             name: '', 
             feedback: ''
@@ -65,8 +77,6 @@ var ReviewControl = React.createClass({
                     <label> Name </label>                   
                     <input type="text" placeholder="Enter Your Name" value={this.state.name} onChange={this.onChangeName} />
                     <br/><br/>
-                    <input type="text" defaultValue="All Things JavaScript"/>
-                    <br/><br/>
                     <label> Feedback </label>
                     <textarea placeholder="Enter Your Feedback" value={this.state.feedback} onChange={this.onChangeFeedback}></textarea>
                     <br/><br/>
@@ -78,6 +88,10 @@ var ReviewControl = React.createClass({
                     <br/><br/>                      
                     <input type="submit" value="Submit" />
                 </form>
+                { /*in order to display the reviews that the user as given, we have created another component called
+                    ReviewCollection. This has property called reviews which is getting its values from getInitialState
+                    method which we defined in our ReviewControl. Once the set of reviews is assigned to this property
+                    the ReviewCollection gets into action */ }
                 <ReviewCollection reviews={this.state.reviews} />
             </div>
         )
@@ -86,14 +100,17 @@ var ReviewControl = React.createClass({
 
 var ReviewCollection = React.createClass({
     render: function() {
+        // here we are making use of the map function and than creating a component called Review with three properties
+        // with course, name, and feedback. we are assiging the properties that we pushed from the submitReview method 
+        // when we were submitting the form thats returned and stored in this variable called reviews.
         var reviews = this.props.reviews.map(function(review){
             return <Review course={review.course} name={review.name} feedback={review.feedback} />
         });
-        
+        // that collection is than returned and showed in the browser
         return (<div> {reviews} </div>)
     }
 })
-
+// the ReviewCollection returns this component with Name, Course, and Feedback
 var Review = React.createClass({
     render: function() {
         return  <div>
