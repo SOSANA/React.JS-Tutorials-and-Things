c2985a334be5fb09a4a498132618b8f033fe8cbb
sosana <thesosana@gmail.com>
2015-06-16 02:03:44 +0000

    updating properties by creating a seperate component that can be used with multiple components and avoids DRY

--- 2.0-masterReactJS/properties/properties.js
+++ 2.0-masterReactJS/properties/properties.js
@@ -1,15 +1,28 @@
 /**
- * Add Notes here
+ * Following shows how to use properties which are external to components and could be used 
+ * to further define that component does
  */
 var Courses = React.createClass({ 
+    // if you need a property that is manditory you can use propTypes with .isRequired and also if 
+    // its a boolean you can add that at well
    propTypes: {
      published:React.PropTypes.bool.isRequired
    },
    getInitialState: function() {
     return {sold: "Not Sold"};
    },
+   // the simplest way to use properties is to define getDefaultProps function and return the properties 
+   // such as date, launch, course, author, students, etc
    getDefaultProps: function() {
-    return { date: new Date("2015-02-14"),launch:" April First Week",course:"Master ReactJS",author:"Skillbakery",students:1,published:false,platform:"Skillbakery" };
+    return { 
+        date: new Date("2015-02-14"),
+        currentDate: new Date,
+        launch:" April First Week",
+        course:"Master ReactJS",
+        author:"Skillbakery",
+        students:1,
+        published:false,
+        platform:"Skillbakery" };
    },
   
    render: function() {
@@ -17,7 +30,9 @@ var Courses = React.createClass({
       <p>
         Course Name: {this.props.course}
         <br/>
-        The Date is {this.props.date.toString()}
+        The original Date {this.props.date.toString()}
+        <br/>
+        The current Date is: {this.props.currentDate.toString()}
         <br/>
         This course is {this.state.sold}
         <br/>
@@ -30,27 +45,34 @@ var Courses = React.createClass({
         Course is {this.props.published.toString()}
         <br/>
         Course will be availble on {this.props.platform}
+        <br/>
+        Course review: {this.props.review}
       </p>
       );
    }
 });
-
+// this component can transfer properties when working with multple components. There may be a situation 
+// where its need to send properties down the structual tree. This has the benefit of reducing code 
+// duplication with DRY (Don't Repeat Yourself)
 var CourseCollection = React.createClass({
   render : function() {
     return (
-      <Courses platform={this.props.platform} />
+      // you can overwrite the getDefaultProps here by passing in new property and value
+      // this another way to define a new property and as well overwrite original property and value
+      <Courses author="Sosana" review="this is not to shabby" platform={this.props.platform} />
     );
   }
 });
 
+// here we are over writing the original property by passing a new property and value
+// and shows how to pass properties
 var courseElement=<CourseCollection platform={"udemy"} />;
 React.render(courseElement,document.getElementById('divContainer'));
 
-
-
-
 /*
+// here is another way to define properties
 var properties={};
+// this will show current date
 properties.date=new Date();
 properties.students=0;
 properties.published=true;
