22876d89a7d26a54537d004732c45aefe7647a0f
sosana <thesosana@gmail.com>
2015-06-19 20:43:31 +0000

    created a new notes js file for jxx compiler

---
+++ 1.0-reactjsNotes/reactjsJSXNotes.js
@@ -0,0 +1,12 @@
+/**
+ * JSX COMPILER IN REACTJS
+ *  - <script src="../js/JSXTransformer.js"></script> is a heavy file and advised to convert it to a 
+ *    normal js file for use in production
+ *  - you can visit https://facebook.github.io/react/jsx-compiler.html for a side by side comparison
+ *  - you can also get the Production CDN URL From http://facebook.github.io/react/downloads.html
+ *  - to make use of the react jsx compiler via npm
+ *      - npm install -g react-tools
+ *      - cd into folder you want to compile jsx
+ *      - in command type following
+ *      > jsx --watch /jsxTools
+ */
