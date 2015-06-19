2151616e58fad36b52e9bf9ce4d2da5b35d8c323
sosana <thesosana@gmail.com>
2015-06-19 19:49:37 +0000

    created notes for accessing the DOM

--- 1.0-reactjsNotes/reactjsAccessingDomNotes.js
+++ 1.0-reactjsNotes/reactjsAccessingDomNotes.js
@@ -1,6 +1,9 @@
 /**
  * ACCESSING DOM IN REACTJS
  *  - check out /dom/dom.js example
+ *  - This allows you to refer and access any element that you have in your reactjs component by using 
+ *    getDOMNode and ref'ing to it.
+ * 
  *  - getDOMNode
  *      - is a function we can call to get reference to components DOMNode. 
  *      - This only works on mounted components. Components that has been placed in the DOM. 
