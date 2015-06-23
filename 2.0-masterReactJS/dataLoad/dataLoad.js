22b1f415914b50c7069f00ea056ba7264740d842
sosana <thesosana@gmail.com>
2015-06-23 18:44:34 +0000

    updating with data components

---
+++ 2.0-masterReactJS/dataLoad/dataLoad.js
@@ -0,0 +1,53 @@
+/*
+ * Using the jquery library with reactjs
+ * Another example of child parent component communication
+ * left off at 3:30
+ */
+var CustomerCollection = React.createClass({
+   getInitialState: function() {
+            return {customer:[]};
+   },
+   componentDidMount: function() {
+    var url="http://services.odata.org/V4/Northwind/Northwind.svc/Customers";
+    $.get(url, function(result) {
+      var data=result;
+      var customerlist = data.value;
+
+      if (this.isMounted()) {
+        this.setState({
+                customer:customerlist
+        });
+      }
+    }.bind(this));
+  },
+   render: function () { 
+      return (<div>
+                    <Customers customer={this.state.customer} />
+            </div>)
+    }
+});
+
+var Customers=React.createClass({
+    render:function(){
+       var customer=this.props.customer.map(function(customer){
+             return <Customer CustomerID={customer.CustomerID} CompanyName={customer.CompanyName} ContactName={customer.ContactName} />
+        });
+   
+       return (<div> {customer} </div>)
+    }
+});
+
+var Customer = React.createClass({
+    render:function(){
+       return <div>
+            <span>Customer ID</span> {this.props.CustomerID}
+            <br/>
+            <span>Company Name</span> {this.props.CompanyName}
+            <br/>
+            <span>Contact Name</span> {this.props.ContactName}
+            <p>&nbsp;</p>
+        </div>
+    }    
+});
+
+React.render(<CustomerCollection  />,document.getElementById('divContainer'));
