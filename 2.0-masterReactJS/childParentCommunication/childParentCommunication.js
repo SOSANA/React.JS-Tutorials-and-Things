473550c1f0df4ad463f697dcae9ca08848b3a284
sosana <thesosana@gmail.com>
2015-06-23 18:39:23 +0000

    updating notes for communication between child and parent events

--- 2.0-masterReactJS/childParentCommunication/childParentCommunication.js
+++ 2.0-masterReactJS/childParentCommunication/childParentCommunication.js
@@ -14,6 +14,8 @@ var courseList=[
 ]
 
 var Courses = React.createClass({ 
+  // here we are defining an event called onSelect
+  // when ever you are selecting any of the courses that is being printed in the console
   onSelect: function(i) {
     console.log('You clicked: ' + this.props.list[i].courseName);
   },
@@ -21,6 +23,15 @@ var Courses = React.createClass({
       return ( 
             <div>
                 We have following courses to offer
+                // here we are using map function to loop through all the courses in the
+                // course list and in doing so we are assigning the onSelect even of the 
+                // parent to this particular child. When we click on any of the courses 
+                // the onSelect in the parent gets called because we are binding that with 
+                // the onClick event. When you click the onClick event is calling the parent
+                // onSelect by this function {this.onSelect.bind(this, i)} and making use of 
+                // the bind event. This bind event is actually binding the select event to
+                // the onclick event of the div. You are calling the parent function using 
+                // the child click event. This way communicates between child and parent
                 { this.props.list.map(function(course, i) {
                     return (
                             <div onClick={this.onSelect.bind(this, i)} key={i}>{course}</div>
@@ -31,5 +42,6 @@ var Courses = React.createClass({
     }
 });
 
+// courses has property called list
 var coursesElement=<Courses list={courseList}/>;
 React.render(coursesElement,document.getElementById('divContainer'));
