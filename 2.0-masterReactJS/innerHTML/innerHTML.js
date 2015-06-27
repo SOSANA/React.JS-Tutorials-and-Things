f008d2402c7024626d8e9ef8950ef3bf99587c08
sosana <thesosana@gmail.com>
2015-06-27 23:24:51 +0000

    avoiding cross site scripting by using dangerouslySetInnerHTML and __html

--- 2.0-masterReactJS/innerHTML/innerHTML.js
+++ 2.0-masterReactJS/innerHTML/innerHTML.js
@@ -1,6 +1,11 @@
 /*
  * Example using dangerously SetInnerHTML
- * 
+ *  - by default reactjs does not allow you to set raw html as the inner html of component. One reason 
+ *    why reactjs does not provide this is do to cross site scripting.
+ *  - We to get around this we use the __html. This returns an html object rather than a plain string. The 
+ *    intent behind using this it will be to pass in the dangerouslySetInnerHTML property and help pass XML
+ *    validation this way you can make use of danerouslySetInnerHTML property in reactjs to make use of
+ *    symbols like the degree symbol used below 
  */
 
 var Weather = React.createClass({
