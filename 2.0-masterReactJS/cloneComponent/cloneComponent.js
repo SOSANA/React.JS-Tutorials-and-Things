603f283c746a2a4fb98877e39ecaa020c4a7e8f9
sosana <thesosana@gmail.com>
2015-06-28 23:58:03 +0000

    updating cloning component and comment notes

--- 2.0-masterReactJS/cloneComponent/cloneComponent.js
+++ 2.0-masterReactJS/cloneComponent/cloneComponent.js
@@ -1,6 +1,12 @@
 /*
- * 
- * 
+ * Example of cloning a reactjs component
+ *  - these are add-ons that are accessible with the react-with-addons-0.13.1.js src file
+ *  - we are using React.addons.cloneWithProps() that accepts a react element parameter and
+ *    as a second argument accepts key and ref. You could of specified a key property in render
+ *    or any unique property that was a part of the orginal element. 
+ *    Example: React.addons.cloneWithProps(formElement, {key:formElement});
+ *  - key and ref are not transfered from the original element to the cloned element. You can 
+ *    preserve the key by passing in the second parameter
  */
 var Form = React.createClass({
    
@@ -20,8 +26,10 @@ var Form = React.createClass({
 });
 
 
-var formElement=<Form />;
-var cloneElement=React.addons.cloneWithProps(formElement});
+var formElement = <Form />;
+// passing in a react element to the parameter
+// can also pass in key property React.addons.cloneWithProps(formElement, {key:formElement.whatever});
+var cloneElement = React.addons.cloneWithProps(formElement);
 
 React.render(formElement,document.getElementById('divContainer'));
 React.render(cloneElement,document.getElementById('divCloneContainer'));
