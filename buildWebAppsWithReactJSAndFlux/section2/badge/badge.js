a9bbb57a716b8c04ac36e90f6e2415e65da0a836
sosana <thesosana@gmail.com>
2015-08-22 18:16:41 +0000

    added props and options object, updated notes

--- buildWebAppsWithReactJSAndFlux/section2/badge/badge.js
+++ buildWebAppsWithReactJSAndFlux/section2/badge/badge.js
@@ -1,17 +1,27 @@
-// define a 'class' using the react method createClass and must always
-// contain a render method. Always capitlize for class
- var Badge = React.createClass({
-   render: function() {
-     return <button class="btn btn-primary" type="button">
-       Messages <span class="badge">4</span>
-     </button>
-   }
- });
+/**
+ * - define a 'class' using the react method createClass and must always
+ *   contain a render method. 
+ * - always make to capitlize for your variable name for classes
+ * - also remember that class is a reserved keyword and there for the use
+ *   of className will render our css class name as shown below. Try
+ * - removing Name form className to show difference when rendering
+ */
+var Badge = React.createClass({
+  render: function() {
+    return <button className="btn btn-primary" type="button">
+      {this.props.title} <span className="badge">{this.props.number}</span>
+    </button>
+  }
+});
 
- // react, please render this instance of our class
- var element = React.createElement(Badge);
-
- // react, after you render this class, please place it in my body tag
- // takes two arugments, instance of class and target to where it should
- // be rendered too
- React.render(element, document.body);
+var options = {
+  title: 'Sent',
+  number: 12
+}
+  
+// react, please render this class
+var element = React.createElement(Badge, options);
+// react, after you render this class, please place it in my body tag
+// takes two arugments, instance of class and target to where it should
+// be rendered too
+React.render(element, document.body);
