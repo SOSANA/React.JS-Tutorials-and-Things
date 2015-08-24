d82ff3d46426b1d8803233d5c2ceada23a0caf2f
sosana <thesosana@gmail.com>
2015-08-24 18:24:15 +0000

    updating notes on react event system

--- 1.0-reactjsNotes/reactjsEventNotes.js
+++ 1.0-reactjsNotes/reactjsEventNotes.js
@@ -1,5 +1,8 @@
 /**
- * EVENTS IN REACTJS
+ * EVENTS IN REACTJS:
+ *  - online src http://facebook.github.io/react/docs/events.html
+ *  - common occurance for events this.handle than whatever event
+ *  - ex: {this.handleChange} or {this.handlKeyPress}
  *  - Clipboard Events
  *          - onCopy 
  *          - onCut 
--- buildWebAppsWithReactJSAndFlux/section4/notes.js
+++ buildWebAppsWithReactJSAndFlux/section4/notes.js
@@ -1,3 +1,6 @@
 /**
-* add notes here
+ * Event System:
+ *  - online src http://facebook.github.io/react/docs/events.html
+ *  - common occurance for events this.handle than whatever event
+ *  - ex: {this.handleChange} or {this.handlKeyPress}
 */
