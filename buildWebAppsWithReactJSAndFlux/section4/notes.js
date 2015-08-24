f03a90ee45ed44936347340404edb150967653d5
sosana <thesosana@gmail.com>
2015-08-24 18:52:10 +0000

    updating notes on event system

--- buildWebAppsWithReactJSAndFlux/section4/notes.js
+++ buildWebAppsWithReactJSAndFlux/section4/notes.js
@@ -2,5 +2,8 @@
  * Event System:
  *  - online src http://facebook.github.io/react/docs/events.html
  *  - common occurance for events this.handle than whatever event
- *  - ex: {this.handleChange} or {this.handlKeyPress}
+ *  - ex: onclick={this.handleChange} or onKeyPress={this.handlKeyPress}
+ *  - keep in mind you can't put event handlers inside another react 
+ *    component, instead it gets passed in as a property. 
+ *    See buildWebAppsWithReactJSAndFlux/section4/dropdown/dropdown.jsx
 */
