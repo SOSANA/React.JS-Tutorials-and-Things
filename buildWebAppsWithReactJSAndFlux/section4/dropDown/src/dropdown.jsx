4f1f7234b3c19b88421372bac1abe01c6441691b
sosana <thesosana@gmail.com>
2015-08-24 20:24:28 -0600

    working with state and events using react method setState() and getInitialState()

--- buildWebAppsWithReactJSAndFlux/section4/dropDown/src/dropdown.jsx
+++ buildWebAppsWithReactJSAndFlux/section4/dropDown/src/dropdown.jsx
@@ -4,21 +4,32 @@
  */
 var React = require('react');
 var Button = require('./button');
-// var List = require('./list');
+var ListItem = require('./list-item');
 
 module.exports = React.createClass({
   handleClick: function() {
-      alert('Hello from dropdown');
+    // using a react method called setState, very important method
+    // what ever your state is reverse it by using "!"
+    this.setState({open: !this.state.open});
+  },
+  getInitialState: function() {
+    return { open: false }
   },
   render: function() {
+    var list = this.props.items.map(function(item) {
+      return <ListItem item={item} />
+    });
 
     return <div className="dropdown" >
-    <Button 
-    whenClicked={this.handleClick} 
-    className="btn-default" 
-    title={this.props.title} 
-    subTitleClassName="caret" 
+    <Button
+    whenClicked={this.handleClick}
+    className="btn-default"
+    title={this.props.title}
+    subTitleClassName="caret"
     />
+  <ul className={"dropdown-menu " + (this.state.open ? "show" : "")}>
+      {list}
+    </ul>
     </div>
   }
 });
