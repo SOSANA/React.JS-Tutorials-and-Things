9ec61cba8baccd87c40137cbc26eb07e444bdb1d
Zach Sosana <thesosana@gmail.com>
2015-08-28 18:55:43 +0000

    wiring up firebase push method for storing our todo text inputs

--- buildWebAppsWithReactJSAndFlux/section5/todos/src/header.jsx
+++ buildWebAppsWithReactJSAndFlux/section5/todos/src/header.jsx
@@ -28,7 +28,17 @@ module.exports = React.createClass({
     // testing click button works
     console.log('the Add button  was clicked!');
     console.log(this.state.text);
-    // send value of text input to firebase
+    
+    // send value of text input to firebase. We immediately make a reference to our
+    // firebase object. We use the 'push' method provided by firebase to create a 
+    // new object in our remote database. Basically were saying hey firebase take 
+    // this data and go and save it online on our database
+    this.props.itemsStore.push({
+      text: this.state.text,
+      done: false
+    });
+    // this basically helps clear out the text input field 
+    this.setState({text: ''});
   },
   handleInputChange: function(event) {
     // testing the event handler for inputing text works
