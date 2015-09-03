a24a98396af3cc789e85e189149913f1c63955ad
Zach Sosana <thesosana@gmail.com>
2015-09-03 18:52:32 +0000

    updating notes on project src files

--- buildWebAppsWithReactJSAndFlux/section6/notes.js
+++ buildWebAppsWithReactJSAndFlux/section6/notes.js
@@ -2,4 +2,7 @@
  * imgur developer api: 
  *  login: https://api.imgur.com/
  *  Api key creds: look at apiKey.js
+ * 
+ * Src code found here:
+ *  https://github.com/StephenGrider/ReactStarter
  */
