f8c85e10e26786713eac53e2cd266926c957dd4e
Zach Sosana <thesosana@gmail.com>
2015-09-05 18:27:50 +0000

    updating

--- buildWebAppsWithReactJSAndFlux/section6/notes.js
+++ buildWebAppsWithReactJSAndFlux/section6/notes.js
@@ -5,6 +5,7 @@
  * 
  * Src code found here:
  *  https://github.com/StephenGrider/ReactStarter
+ *  https://github.com/StephenGrider/ReactCasts
  * 
  * install the following for react=router version:
 *   - $ npm install react-router@1.0.0-beta2 --save
