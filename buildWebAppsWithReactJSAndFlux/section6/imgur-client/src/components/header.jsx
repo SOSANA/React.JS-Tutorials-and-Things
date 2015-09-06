8ca5311d8c81fbeab0238e5b1522f9ed46e5a1f1
Zach Sosana <thesosana@gmail.com>
2015-09-06 20:19:48 +0000

    created new header component

---
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/header.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return <nav className="navbar navbar-default header">
+      <div className="container-fluid">
+        <a href="/" className="navbar-brand">
+          Imgur browser
+        </a>
+        <ul className="nav navbar-nav navbar-right>
+          <li> <a>Topic #1</a> </li>
+        </ul>
+      </div>
+    </nav>
+  }
+});
