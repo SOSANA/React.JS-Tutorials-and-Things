94cdf4345048f65be8f865b06baf88d1fce4b687
sosana <thesosana@gmail.com>
2015-09-07 01:12:55 -0600

    updating

--- buildWebAppsWithReactJSAndFlux/src/imgur-client/src/components/main.jsx
+++ buildWebAppsWithReactJSAndFlux/src/imgur-client/src/components/main.jsx
@@ -1,10 +1,9 @@
 var React = require('react');
-var Header = require('./header');
 
 module.exports = React.createClass({
   render: function() {
     return <div>
-      I am a header.
+      I'm a header.
       {this.props.children}
     </div>
   }
--- buildWebAppsWithReactJSAndFlux/src/imgur-client/src/routes.jsx
+++ buildWebAppsWithReactJSAndFlux/src/imgur-client/src/routes.jsx
@@ -1,14 +1,11 @@
 var React = require('react');
-// This is the actual react router library
 var ReactRouter = require('react-router');
-// an object that tells the router how we will be keeping track of what page the user is on at any given time
 var HashHistory = require('react-router/lib/hashhistory');
-// is the actual Router that will be deciding what content to show on the page at any given time
 var Router = ReactRouter.Router;
-// is an object that will be used to configure the router
 var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
+
 module.exports = (
   <Router history={new HashHistory}>
     <Route path="/" component={Main}>
