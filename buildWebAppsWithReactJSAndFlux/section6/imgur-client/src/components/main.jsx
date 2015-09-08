5de5d7ecc071211e0737c0c595fa75a12af52a77
Zach Sosana <thesosana@gmail.com>
2015-09-08 01:49:36 +0000

    adding a temp variable called renderCount to test component rendering

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/main.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/main.jsx
@@ -1,8 +1,14 @@
 var React = require('react');
 var Header = require('./header');
 
+// a way to test our component is rendering
+//var renderCount = 0;
+
 module.exports = React.createClass({
   render: function() {
+    // logging out our renderCount to test we rendering when click on Imgur Browser
+    //console.log("I've been rendered " + renderCount++ + " f*cken times with no full page refresh!!");
+    
     return <div>
       <Header />
       {this.props.children}
