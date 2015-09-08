bdb0d7164a0d916273286568cab45e75bdc6da66
Zach Sosana <thesosana@gmail.com>
2015-09-08 01:51:28 +0000

    updated header with Router.Link to replace anchor tags and href so app doesn't do full page refresh but instead renders components

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/header.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/header.jsx
@@ -1,12 +1,16 @@
 var React = require('react');
+var Router = require('react-router');
+// where ever we see an anchor tag <a></a> will put Link instead and instead of href
+// we'll assign a to
+var Link = Router.Link;
 
 module.exports = React.createClass({
   render: function() {
     return <nav className="navbar navbar-default header">
       <div className="container-fluid">
-        <a href="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand">
           Imgur browser
-        </a>
+        </Link>
         <ul className="nav navbar-nav navbar-right">
           <li> <a>Topic #1</a> </li>
         </ul>
