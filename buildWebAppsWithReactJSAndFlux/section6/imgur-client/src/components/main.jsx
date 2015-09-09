93bbd82f06fd42003544f0d524b5304111341a9c
Zach Sosana <thesosana@gmail.com>
2015-09-09 18:38:14 +0000

    added require TopicList component, added new method content, and updated our render object

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/main.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/main.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var Header = require('./header');
+var TopicList = require('./topic-list');
 
 // a way to test our component is rendering
 //var renderCount = 0;
@@ -8,10 +9,17 @@ module.exports = React.createClass({
   render: function() {
     // logging out our renderCount to test we rendering when click on Imgur Browser
     //console.log("I've been rendered " + renderCount++ + " f*cken times with no full page refresh!!");
-    
+
     return <div>
       <Header />
-      {this.props.children}
+      {this.content()}
     </div>
+  },
+  content: function() {
+    if(this.props.children) {
+      return this.props.children
+    } else {
+      return <TopicList />
+    }
   }
 });
