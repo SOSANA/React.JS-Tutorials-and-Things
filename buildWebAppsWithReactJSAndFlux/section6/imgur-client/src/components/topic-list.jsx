562b781933803c68c8bf434ffacfe968519c131d
Zach Sosana <thesosana@gmail.com>
2015-09-13 04:10:52 +0000

    updating

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/topic-list.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/topic-list.jsx
@@ -1,9 +1,32 @@
 var React = require('react');
+var Api = require('../utils/api');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function() {
+    Api.get('topics/defaults')
+    // our function has a reference to 'this'
+    .then(function(data) {
+      this.setState({
+        // so happens that our object data we created has a key called data 
+        topics: data.data
+      });
+    }.bind(this));
+  },
   render: function() {
     return <div className="list-group">
       Topic List
     </div>
+  },
+  renderTopics: function() {
+    return this.state.topics.map(function(topic) {
+      return <li>
+        {topic}
+      </li>
+    });
   }
-})
+});
