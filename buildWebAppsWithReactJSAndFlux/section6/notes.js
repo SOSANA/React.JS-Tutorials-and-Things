e9d665d526810ad6f2fa33ea6bca8ba97f73686a
Zach Sosana <thesosana@gmail.com>
2015-09-16 00:37:07 +0000

    adding notes about passing props and minimizing fetching of data

--- buildWebAppsWithReactJSAndFlux/section6/notes.js
+++ buildWebAppsWithReactJSAndFlux/section6/notes.js
@@ -1,4 +1,16 @@
 /**
+ * remember the following: 
+ *  - the only way we can pass data between components is through 'this.props' object
+ *    and the component needs to render its child component to have access
+ *  - 'componentWillMount:' always runs right before the component is rendered and only 
+ *    gets rendered one time. If there is any child or other components that need to get access to 
+ *    this data we will have to fetch that data in another component due to not being able to pass
+ *    'this.props' to it because we are not rendering that component. Long story short if any of 
+ *    our components need access to our topic's or data we are going to have to re-fetch data and
+ *    we would have to duplicate code throught our app
+ *      ex: you re-render the page because your route changes or click on another component 
+ *          it needs to re fetch the data
+ * 
  * imgur developer api: 
  *  login: https://api.imgur.com/
  *  Api key creds: look at apiKey.js
