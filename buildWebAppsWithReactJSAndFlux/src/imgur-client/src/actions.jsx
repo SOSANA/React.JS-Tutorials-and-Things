2549cd3635a8789458e34997fbc11d29c8ea0d09
Zach Sosana <thesosana@gmail.com>
2015-09-24 00:46:59 +0000

    creating topic page

--- buildWebAppsWithReactJSAndFlux/src/imgur-client/src/actions.jsx
+++ buildWebAppsWithReactJSAndFlux/src/imgur-client/src/actions.jsx
@@ -1,6 +1,5 @@
 var Reflux = require('reflux');
 
-//This will trigger a 'getTopics' and 'getImages' method on either topic-store or image-store
 module.exports = Reflux.createActions([
   'getTopics',
   'getImages'
