c0de6c98f0553f0847f9d18206ed17b514df01b1
Zach Sosana <thesosana@gmail.com>
2015-09-24 00:45:30 +0000

    created topic component

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/topic.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/topic.jsx
@@ -1,9 +1,27 @@
 var React = require('react');
+var Actions = require('../actions/topic-action');
+var ImageStore = require('../stores/image-store');
+var Reflux = require('reflux');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+  getInitialState: function() {
+    return {
+      images: []
+    };
+  },
+  componentWillMount: function() {
+    Actions.getImages(this.props.params.id);
+  },
   render: function() {
     return <div>
-      I am a topic!
+      I am a topic with ID {this.props.params.id}
+      
     </div>
+  },
+  onChange: function(event, images) {
+    this.setState({images: images});
   }
 })
