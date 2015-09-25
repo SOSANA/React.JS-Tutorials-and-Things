00db6ddfd01f6e7cbdc8231af116479da400a10b
Zach Sosana <thesosana@gmail.com>
2015-09-25 18:50:24 +0000

    updating with componentWillReceiveProps method

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/topic.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/topic.jsx
@@ -12,10 +12,23 @@ module.exports = React.createClass({
       images: []
     };
   },
+  // componentWillMount Method is invoked once both on the client and on the server
   componentWillMount: function() {
+    // for testing
+    console.log('Topic is about to render and fetch data');
     Actions.getImages(this.props.params.id);
   },
+  // componentWillReceiveProps is invoked when a component is receiving new properties. 
+  // This is a react method that gets called with an object called 'nextProps'. 'nextProps' 
+  // means whenever this function is called, we are about t get new set of properties and 
+  // when that happens you have to rerender yourself
+  componentWillReceiveProps: function(nextProps) {
+    Actions.getImages(nextProps.params.id);
+  },
   render: function() {
+    // for testing
+    console.log('Topic is rendering with ID', this.props.params.id);
+    console.log('I have this many images', this.state.images.length);
     return <div>
       I am a topic with ID {this.props.params.id}
       
