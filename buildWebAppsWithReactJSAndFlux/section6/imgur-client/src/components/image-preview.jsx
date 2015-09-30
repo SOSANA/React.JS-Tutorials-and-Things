c0faed0fae84828f7de633680a93a3ade6b369db
Zach Sosana <thesosana@gmail.com>
2015-09-30 00:30:49 +0000

    updating to apply image method

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/image-preview.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/image-preview.jsx
@@ -3,7 +3,13 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return <div>
-      I am an image with id {this.props.id}
+      { this.image() }
     </div>
+  },
+  image: function() {
+    // 'h' represent the quality of the pixal, h is 200x200 Pixel
+    var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg';
+    
+    return <img src={link} />
   }
 });
