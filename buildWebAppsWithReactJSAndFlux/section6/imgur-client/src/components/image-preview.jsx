b252334bdc1460483d30e170586ca266eb397a58
Zach Sosana <thesosana@gmail.com>
2015-10-01 18:55:10 +0000

    adding mouse enter and leave handler, upvotes and views inset

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/image-preview.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/image-preview.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    hovering: false
+  },
   render: function() {
-    return <div>
-      { this.image() }
+    // react provides onMouseEnter & onMouseLeave
+    return <div 
+    className="image-preview"
+    onMouseEnter={this.handleMouseEnter}
+    onMouseLeave={this.handleMouseLeave}
+    >
+      { this.props.animated && this.state.hovering ? this.video() : this.image() }
+      { this.props.animated && !this.state.hovering ? this.icon() : null }
+      { this.state.hovering ? this.inset() : null}
     </div>
   },
   image: function() {
@@ -11,5 +21,29 @@ module.exports = React.createClass({
     var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg';
     
     return <img src={link} />
+  },
+  video: function() {
+    // in video element providing html5 properties
+    return <div>
+      <video preload='auto' autoPlay='autoplay' loop='loop' webkit-playsinline>
+        <source src={this.props.mp4} type='video/mp4'></source>
+      </video>
+    </div>
+  },
+  inset: function() {
+    return <div className="inset">
+    Views: {this.props.views}
+    <br />
+    Upvotes: {this.props.ups}
+    </div>
+  },
+  icon: function() {
+    return <span className="glyphicon glyphicon-play"></span>
+  },
+  handleMouseEnter: function() {
+    this.setState({hovering: true});
+  },
+  handleMouseLeave: function() {
+    this.setState({hovering: false});
   }
 });
