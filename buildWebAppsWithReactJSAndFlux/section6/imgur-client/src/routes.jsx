4c23ea2fe93e8226fa1a0012aea94438300ab769
Zach Sosana <thesosana@gmail.com>
2015-10-02 19:00:07 +0000

    updating routes to include new route to image desc

--- buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/routes.jsx
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/routes.jsx
@@ -10,13 +10,14 @@ var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
 var Topic = require('./components/topic');
+var ImageDetail = require('./components/image-detail');
 
 module.exports = (
   <Router history={new HashHistory}>
     <Route path="/" component={Main}>
       {/* using '/:' for parameters */}
       <Route path="topics/:id" component={Topic} />
-
+      <Route path="images/:id" component={ImageDetail} />
     </Route>
   </Router>
 )
