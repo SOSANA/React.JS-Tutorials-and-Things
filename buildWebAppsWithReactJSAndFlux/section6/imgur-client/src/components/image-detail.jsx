16129ca26d9edcc015e2c7eeaa6ee24d60f3bc7a
Zach Sosana <thesosana@gmail.com>
2015-10-02 19:01:55 +0000

    created new component to show image detail

---
+++ buildWebAppsWithReactJSAndFlux/section6/imgur-client/src/components/image-detail.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var Reflux = require('reflux');
+var ImageStore = require('../stores/image-store');
+var Actions = require('../actions/topic-action');
+
+module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+  getInitialState: function() {
+    return {
+      image: null
+    }
+  },
+  componentWillMount: function() {
+    Actions.getImage(this.props.params.id);
+  },
+  render: function () {
+    return <div>
+      {this.state.image}
+    </div>
+  },
+  onChange: function(){
+    this.setState({
+      image: ImageStore.find(this.props.params.id)
+    });
+  }
+});
