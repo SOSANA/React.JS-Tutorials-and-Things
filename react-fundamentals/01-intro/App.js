194d927d964a66bb4572e32a6a2512c0f844428c
sosana <thesosana@gmail.com>
2016-01-05 15:54:18 -0700

    class vs stateless components

--- react-fundamentals/01-intro/App.js
+++ react-fundamentals/01-intro/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+
+// one way to create a component, called a 'class component' which has state
+/*
 class App extends React.Component {
   render() {
-    return <div>Hi</div>;
+    return <h1>Hello World</h1>;
   }
 }
+*/
+
+// another way to create a component called a 'stateless component' which will
+// not have state
+const App = () => <h1>Hello Guys</h1>;
 
 export default App;
