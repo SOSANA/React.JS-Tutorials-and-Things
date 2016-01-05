db25aeb240dfc286a0ddc6cb275fab4cf29bf2ba
sosana <thesosana@gmail.com>
2016-01-05 16:03:41 -0700

    using the render method

---
+++ react-fundamentals/02-render/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+// one way to create a component, called a 'class component' which has state
+// can only return one element
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World, again</h1>
+        <b>Bold</b>
+      </div>
+    );
+  }
+}
+
+// without parentheses, first element tag needs to be on return line
+// either way is prference
+/*
+class App extends React.Component {
+  render() {
+    return <div>
+        <h1>Hello World, again part deux</h1>
+        <b>Bold</b>
+        </div>;
+
+  }
+}
+*/
+
+export default App;
