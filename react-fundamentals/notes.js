96e87ffe99ec93dcb970c750c928454fd48bc7fc
sosana <thesosana@gmail.com>
2016-01-05 15:45:04 -0700

    updating

---
+++ react-fundamentals/notes.js
@@ -0,0 +1,6 @@
+/**
+ * Development Environment Setup:
+ * 	- $ npm i react react-dom -S
+ * 	- $ npm i babel-loader babel-preset-es2015 babel-preset-react -S
+ * 	- $ npm i babel webpack webpack-dev-server -g
+ */
