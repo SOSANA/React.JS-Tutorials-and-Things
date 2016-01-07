4c1668ac1b73b358be0fc0de4fe42306423bc402
sosana <thesosana@gmail.com>
2016-01-07 15:57:41 -0700

    adding example of using refs with react

---
+++ react-fundamentals/06-refs/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    // super gives us our context for 'this'
+    super();
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+    this.update = this.update.bind(this);
+  }
+
+  // creating a custom method
+  update(e) {
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+          {this.state.green}
+          <br />
+        <Slider ref="blue" update={this.update} />
+          {this.state.blue}
+          <br />
+      </div>
+    );
+  }
+}
+
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="inp" type ="range"
+          min="0"
+          max="255"
+          onChange={this.props.update} />
+      </div>
+    );
+  }
+}
+
+// creating a stateless component, ***ref's won't work with stateless components
+/*
+const Widget = (props) => {
+  return (
+    <div>
+      <input type ="text"
+        onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
+};
+*/
+
+export default App;
