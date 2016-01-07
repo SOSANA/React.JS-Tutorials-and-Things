77202de8f317c1651632b0bbd338596774df3493
sosana <thesosana@gmail.com>
2016-01-07 15:19:44 -0700

    updating comments

--- react-fundamentals/04-state/main.js
+++ react-fundamentals/04-state/main.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(
-  <App cat={5} />, document.getElementById('app'));
 
-  // this would override defaultProps
-  //<App cat={5} txt="This is the props value"/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
