881eda2405e4b32bf265cfb2f7cca0e8dd5353c0
sosana <thesosana@gmail.com>
2016-01-07 15:29:59 -0700

    adding example of owner-ownee parent child relationship

---
+++ react-fundamentals/05-owner-ownee/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    // super gives us our context for 'this'
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0,
+    };
+    this.update = this.update.bind(this);
+  }
+
+  // creating a custom method
+  update(e) {
+    this.setState({ txt: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+        <Widget txt={this.state.txt} update={this.update} />
+      </div>
+    );
+  }
+}
+
+// creating a stateless component
+const Widget = (props) => {
+  return (
+    <div>
+      <input type ="text"
+        onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
+};
+
+export default App;
