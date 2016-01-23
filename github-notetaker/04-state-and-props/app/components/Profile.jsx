744281e2307cb02b98daeef623574f77998ff09b
sosana <thesosana@gmail.com>
2016-01-22 17:21:03 -0700

    importing new child components and updating render methods

--- github-notetaker/04-state-and-props/app/components/Profile.jsx
+++ github-notetaker/04-state-and-props/app/components/Profile.jsx
@@ -1,5 +1,8 @@
 var React = require('react');
 var Router = require('react-router');
+var Repos = require('./Github/Repos');
+var UserProfile = require('./Github/UserProfile');
+var Notes = require('./Notes/Notes');
 
 var Profile = React.createClass({
   getInitialState: function() {
@@ -17,13 +20,13 @@ var Profile = React.createClass({
     return (
       <div className="row">
         <div className="col-md-4">
-          User Profile Component
+          <UserProfile username={this.props.params.username} bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          Repos Component
+          <Repos repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          Notes Component
+          <Notes notes={this.state.notes} />
         </div>
       </div>
     );
