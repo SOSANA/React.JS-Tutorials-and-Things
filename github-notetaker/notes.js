7f71825229963bcbdc03c559913b77e90053f8a2
sosana <thesosana@gmail.com>
2016-01-25 14:11:16 -0700

    updating notes on installing dependencies

--- github-notetaker/notes.js
+++ github-notetaker/notes.js
@@ -1,17 +1,19 @@
 /**
  * install the following:
  * 	- $ npm init
- * 	- $ npm install webpack -g
- * 	- $ npm install --S react
- * 	- $ npm install --S react-dom
- * 	- $ npm install --S react-router
- * 	- $ npm install --S history
- * 	- $ npm install --S babel-register // for registering babel on server and using es6
- * 	- $ npm install --S react-engine // for server side react rendering
- * 	- $ npm install --save-dev babel-core
- * 	- $ npm install --save-dev babel-loader
- * 	- $ npm install --save-dev babel-preset-es2015
- * 	- $ npm install --save-dev babel-preset-react
+ * 	- $ npm i webpack -g
+ * 	- $ npm i -S react
+ * 	- $ npm i -S react-dom
+ * 	- $ npm i -S react-router
+ * 	- $ npm i -S history
+ * 	- $ npm i -S babel-register // for registering babel on server and using es6
+ * 	- $ npm i -S react-engine // for server side react rendering
+ * 	- $ npm i -S reactfire // for using react with firebase
+ * 	- $ npm i -S firebase // for firebase
+ * 	- $ npm i --save-dev babel-core
+ * 	- $ npm i --save-dev babel-loader
+ * 	- $ npm i --save-dev babel-preset-es2015
+ * 	- $ npm i --save-dev babel-preset-react
  *
  * Start webpack:
  * 	- $ webpack -w
