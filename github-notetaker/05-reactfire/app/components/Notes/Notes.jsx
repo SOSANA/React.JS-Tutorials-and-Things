95f3768b3a5be07300c0ca12a4606407b455346b
sosana <thesosana@gmail.com>
2016-01-25 14:11:57 -0700

    adding project for implementing firebase with react

---
+++ github-notetaker/05-reactfire/app/components/Notes/Notes.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var NotesList = require('./NotesList');
+
+var Notes = React.createClass({
+  render: function() {
+    console.log('Notes: ', this.props.notes);
+    return (
+      <div>
+        <h3>Notes for {this.props.username}</h3>
+        <NotesList notes={this.props.notes} />
+      </div>
+    );
+  },
+});
+
+module.exports = Notes;
