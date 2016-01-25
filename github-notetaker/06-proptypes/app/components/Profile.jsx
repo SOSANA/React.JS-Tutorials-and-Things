b7bcbc9e3d19539998038a25f1fd7af5f2fb5136
sosana <thesosana@gmail.com>
2016-01-25 14:49:42 -0700

    adding example of using PropTypes to make sure our types validate and are being used correctly

---
+++ github-notetaker/06-proptypes/app/components/Profile.jsx
@@ -0,0 +1,65 @@
+var React = require('react');
+var Router = require('react-router');
+var Repos = require('./Github/Repos');
+var UserProfile = require('./Github/UserProfile');
+var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
+
+var Profile = React.createClass({
+  mixins: [ReactFireMixin],
+
+  getInitialState: function() {
+    return {
+      notes: [1, 2, 3],
+      bio: {
+        name: 'The Sosana',
+      },
+      repos: ['a', 'b', 'c'],
+    };
+  },
+
+  // this where you want to do your ajax requests and firebase listeners
+  // 'componentDidMount' lifecycle event will be called right after your
+  // component mounts the view
+  componentDidMount: function() {
+    // creating a new instance of firebase which will return you a new object
+    // full firebase e type properties and save that on your instance under
+    // the ref property
+    this.ref = new Firebase('https://sosana-note-taker.firebaseio.com/');
+
+    // take our ref and add what ever username passed the url above .../:username
+    var childRef = this.ref.child(this.props.params.username);
+
+    // this is from reactfire, takes two arugments, the first is a ref to your
+    // firebase and the second argument is the property on  your state that you
+    // want to bind your firebase too
+    this.bindAsArray(childRef, 'notes');
+  },
+
+  componentWillUnmount: function() {
+    // calling the 'unbind' property on reactfire and pass it notes
+    // This removes that listener so that we are not always listening and not
+    // always trying to update our state even after we out component has moved
+    // on
+    this.unbind('notes');
+  },
+
+  render: function() {
+    return (
+      <div className="row">
+        <div className="col-md-4">
+          <UserProfile username={this.props.params.username} bio={this.state.bio} />
+        </div>
+        <div className="col-md-4">
+          <Repos username={this.props.params.username} repos={this.state.repos} />
+        </div>
+        <div className="col-md-4">
+          <Notes username={this.props.params.username} notes={this.state.notes} />
+        </div>
+      </div>
+    );
+  },
+});
+
+module.exports = Profile;
