dd36782b49a230ef82e5392036a193f81b0ff23f
sosana <thesosana@gmail.com>
2016-01-26 17:02:51 -0700

    adding example project using axios for github http requests

---
+++ github-notetaker/09-axios/app/components/Github/Repos.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var Repos = React.createClass({
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    repos: React.PropTypes.array.isRequired,
+  },
+  render: function() {
+    console.log('REPOS', this.props.repos);
+    return (
+      <div>
+        <p> REPOS </p>
+      </div>
+    );
+  },
+});
+
+module.exports = Repos;
---
+++ github-notetaker/09-axios/app/components/Github/UserProfile.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var UserProfile = React.createClass({
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    bio: React.PropTypes.object.isRequired,
+  },
+  render: function() {
+    console.log('BIO', this.props.bio);
+    return (
+      <div>
+        <p> USER PROFILE! </p>
+        <p> Username: {this.props.username} </p>
+      </div>
+    );
+  },
+});
+
+module.exports = UserProfile;
