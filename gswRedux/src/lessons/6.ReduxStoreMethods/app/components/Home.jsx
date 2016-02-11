41424c16f457570ca8f87553d913be1e4d0a5bbb
sosana <thesosana@gmail.com>
2016-02-10 17:21:04 -0700

    updating example project for working with redux store methods

--- gswRedux/src/lessons/6.ReduxStoreMethods/app/components/Home.jsx
+++ gswRedux/src/lessons/6.ReduxStoreMethods/app/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 // stateless function - only has a render method or no methods at all
 const Home = () => {
   return (
-    <h1 className="text-left">
+    <h1 className="text-center">
       Hello World
     </h1>
   );
--- gswRedux/src/lessons/6.ReduxStoreMethods/app/config/routes.jsx
+++ gswRedux/src/lessons/6.ReduxStoreMethods/app/config/routes.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Home from '../components/Home';
-import { Route, IndexRoute } from 'react-router';
+import { Route } from 'react-router';
 
 export default (
   <Route path="/" component={Home}>
-    <IndexRoute component={Home} />
   </Route>
 );
