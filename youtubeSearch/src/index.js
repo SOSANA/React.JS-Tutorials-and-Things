0bf34724e33ae7369541505a8f61ba5e4a0f5290
sosana <thesosana@gmail.com>
2016-06-16 18:54:27 -0600

    adding stateless component

--- youtubeSearch/src/index.js
+++ youtubeSearch/src/index.js
@@ -1,3 +1,13 @@
 /**
- * add notes here
+ * tasks:
+ *  [x] create a new component, this component should produce some html
  */
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return <div> Hi!</div>;
+}
+
+// making an instance of our component
+ReactDom.render(<App />, document.querySelector('.container'));
