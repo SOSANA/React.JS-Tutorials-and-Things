c5b9e1580069da6be76c43bf68f63087832c173a
SOSANA <thesosana@gmail.com>
2016-06-20 17:25:49 -0600

    updating props for child component and adding in new elements to for thumbnails and title

--- youtubeSearch/src/components/video_list_item.js
+++ youtubeSearch/src/components/video_list_item.js
@@ -1,3 +1,22 @@
-/**
- * 
- */
+import React from 'react';
+
+// passing video same as passing in props parameter and setting variable to const video = props.video;
+const VideoListItem = ({ video }) => { // eslint-disable-line
+  // console.log(video); // looking up object to get info for imageUrl and video title
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
