526e5ca7a41b35157bd7ec3956b66a69fdeea4e0
SOSANA <thesosana@gmail.com>
2016-06-23 13:19:43 -0600

    updating naming convention

--- bookList/src/components/app.js
+++ bookList/src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import BookList from '../containers/bookList';
+import BookList from '../containers/bookListContainer';
 
 export default class App extends Component {
   render() {
--- bookList/src/reducers/index.js
+++ bookList/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducerBooks';
+import BooksReducer from './booksReducer';
 
 const rootReducer = combineReducers({
   // key/value where key is the name of the state and the value is the name of the reducer
