c615d24ca8c3f42d0f4b57bcf414b8383c8ee184
SOSANA <thesosana@gmail.com>
2016-06-23 13:06:57 -0600

    adding notes on containers

---
+++ bookList/src/containers/containerNotes.js
@@ -0,0 +1,10 @@
+/**
+ * creating a container:
+ *  - is a react component that has direct connection to the state managed by redux
+ *  - forms the bridge between react and redux, in our mockup its part of that app
+ *  	that joins these two libraries
+ *  - we inject data/state into a container
+ *  - also called smart component
+ *  - our most parent component should be our container
+ *  - the react-redux module is the glue between react and redux
+ */
