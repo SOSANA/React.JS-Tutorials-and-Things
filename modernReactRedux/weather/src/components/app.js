173ace0be799b16ec2b4ce31b80266cda0274e18
SOSANA <thesosana@gmail.com>
2016-06-25 16:09:49 -0600

    updating to include search bar component

--- modernReactRedux/weather/src/components/app.js
+++ modernReactRedux/weather/src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/searchBar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
