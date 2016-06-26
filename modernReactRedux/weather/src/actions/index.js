cde3c6c1e96525ef785587208fb529617e5cf018
SOSANA <thesosana@gmail.com>
2016-06-25 19:47:09 -0600

    wiring up our ajax requests with axios

--- modernReactRedux/weather/src/actions/index.js
+++ modernReactRedux/weather/src/actions/index.js
@@ -4,11 +4,14 @@ import { API_KEY, ROOT_URL } from '../../misc/API_KEY';
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&${city},ca`;
-  const request = axios.get(url);
+  // search query
+  const url = `${ROOT_URL}&q=${city},us`;
+  // creating an axios ajax request in the form a 'get' request and returns a
+  // promise, than we pass that promise into the actions payload property
+  const REQUEST = axios.get(url);
 
   return {
     type: FETCH_WEATHER,
-    payload: request,
+    payload: REQUEST,
   };
 }
