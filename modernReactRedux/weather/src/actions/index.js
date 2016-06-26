84815e7117d9a4c90cb3d5678a70047e7392a095
SOSANA <thesosana@gmail.com>
2016-06-25 18:48:46 -0600

    setting up our action creator and axios

--- modernReactRedux/weather/src/actions/index.js
+++ modernReactRedux/weather/src/actions/index.js
@@ -1,1 +1,14 @@
-import API_KEY from '../../misc/API_KEY';
+import axios from 'axios';
+import { API_KEY, ROOT_URL } from '../../misc/API_KEY';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&${city},ca`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
