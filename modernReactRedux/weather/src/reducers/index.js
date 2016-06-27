9f68f675e307d9aad72fb1500aa2d2f58010f9ec
SOSANA <thesosana@gmail.com>
2016-06-27 14:03:25 -0600

    updating index combineReducers property

--- modernReactRedux/weather/src/reducers/index.js
+++ modernReactRedux/weather/src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import WeatherReducer from './weatherReducer';
 
 const rootReducer = combineReducers({
-  Weather: WeatherReducer,
+  weather: WeatherReducer,
 });
 
 export default rootReducer;
