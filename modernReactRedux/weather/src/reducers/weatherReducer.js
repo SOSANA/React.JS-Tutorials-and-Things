e08814d9d61a5f7fad7e570352a30f95ea8624a6
SOSANA <thesosana@gmail.com>
2016-06-27 13:37:53 -0600

    updated to handle action creater FETCH_WEATHER

--- modernReactRedux/weather/src/reducers/weatherReducer.js
+++ modernReactRedux/weather/src/reducers/weatherReducer.js
@@ -1,5 +1,14 @@
-export default function (state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
   // also uncomment reducer to see action creator cycle
   // console.log('Action received', action);
+  switch (action.type) { // eslint-disable-line
+    case FETCH_WEATHER:
+      // returning a new instance of our state, not mutating our state
+      // return state.concat([action.payload.data]);
+      // insteaed of writing state.concat we use new es6 spread operator
+      return [action.payload.data, ...state];
+  }
   return state;
 }
