a287bbe2c7555403a740f6d630516c970537e8c0
SOSANA <thesosana@gmail.com>
2016-06-27 13:37:04 -0600

    updating to include WeatherList container

--- modernReactRedux/weather/src/components/app.js
+++ modernReactRedux/weather/src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/searchBar';
+import WeatherList from '../containers/WeatherList';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
