e3ebcc396fabebc9f62f5f6fc5fc89c71e3c6a8d
SOSANA <thesosana@gmail.com>
2016-06-27 15:23:10 -0600

    updating chart margin avg of weather

--- modernReactRedux/weather/src/components/chart.js
+++ modernReactRedux/weather/src/components/chart.js
@@ -1,13 +1,21 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 // importing sparklines component module
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+
+function average(data) {
+  return _.round(_.sum(data) / data.length);
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
     </div>
   );
 };
