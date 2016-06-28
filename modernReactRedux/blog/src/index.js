3a7f0391b9a7e01275d16fa33c65d1314eaf2528
SOSANA <thesosana@gmail.com>
2016-06-28 14:55:34 -0600

    wiring up our redux-promise and browser history for react-router

--- modernReactRedux/blog/src/index.js
+++ modernReactRedux/blog/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import promise from 'redux-promise';
 
-import App from './components/app';
+import routes from './routes';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
