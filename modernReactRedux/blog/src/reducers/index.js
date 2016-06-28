e00bfd552089672bba74f84c8ba93a49311dd64d
SOSANA <thesosana@gmail.com>
2016-06-28 15:11:04 -0600

    wiring up our fetch posts reducer

--- modernReactRedux/blog/src/reducers/index.js
+++ modernReactRedux/blog/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import PostsReducer from './postsReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducer,
 });
 
 export default rootReducer;
