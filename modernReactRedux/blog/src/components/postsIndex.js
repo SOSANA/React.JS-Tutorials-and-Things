2032618d500ea7bfedf3e4c850f35241f3e19094
SOSANA <thesosana@gmail.com>
2016-06-28 21:26:39 -0600

    wiring up life cycle method to fetch data

--- modernReactRedux/blog/src/components/postsIndex.js
+++ modernReactRedux/blog/src/components/postsIndex.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux'; // don't need this as per avoid boilerplate below
+// this is the action creator we want to call inside of componentWillMount
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
-export default () => {
-  return <div>List of blog posts.</div>;
-};
+class PostsIndex extends Component {
+  // react lifecycle method to call an action creator to fetch our data (posts)
+  // remember only called once at time of render
+  componentWillMount() {
+    // console.log('this will be good time to call an action creator to fetch posts');
+    // action creator
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+          List of blog posts.
+      </div>
+    );
+  }
+}
+
+/*
+function mapDispatchToProps(dispatch) {
+  // remember this give us access to this.props.fetch.post
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
+*/
+
+// avoiding above boilerplate
+export default connect(null, { fetchPosts })(PostsIndex);
