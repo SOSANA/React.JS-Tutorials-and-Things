98295ba7ffd33c2a6f3105bdc41d96121f08aa3b
SOSANA <thesosana@gmail.com>
2016-06-28 21:27:38 -0600

    adding route for creating a new post

--- modernReactRedux/blog/src/routes.js
+++ modernReactRedux/blog/src/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/postsIndex';
+import NewPost from './components/newPost';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
+    <Route path="posts/new" component={NewPost} />
   </Route>
 );
