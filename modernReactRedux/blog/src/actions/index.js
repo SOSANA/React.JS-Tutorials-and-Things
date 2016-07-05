039ee6043e4dc445dd3f0dbce6ea5673217d0376
SOSANA <thesosana@gmail.com>
2016-07-05 14:59:26 -0600

    adding new createPost action creator for redux-form

--- modernReactRedux/blog/src/actions/index.js
+++ modernReactRedux/blog/src/actions/index.js
@@ -1,16 +1,29 @@
 import axios from 'axios';
 
+// our constaints
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 // normally store this in hidden config file FYI
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=randomCharacters';
 
+// action creator to fetch our posts
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request,
+  };
+}
+
+// action creator to handle our redux-form
+export function createPost(props) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
     payload: request,
   };
 }
