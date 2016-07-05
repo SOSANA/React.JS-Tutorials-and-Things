2cfe14023492fc299af9052a7dcfbe2b50c114fc
SOSANA <thesosana@gmail.com>
2016-07-05 15:00:36 -0600

    adding our form reducer

--- modernReactRedux/blog/src/reducers/index.js
+++ modernReactRedux/blog/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './postsReducer';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
