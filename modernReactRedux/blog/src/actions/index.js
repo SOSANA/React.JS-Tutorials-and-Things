959b845c266cc9697b6d9de409d4523534e29c85
SOSANA <thesosana@gmail.com>
2016-07-06 13:51:54 -0600

    adding action creators and constaints for DELETE_POST and FETCH_POST

--- modernReactRedux/blog/src/actions/index.js
+++ modernReactRedux/blog/src/actions/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-// our constaints
+// our constaint type's
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 // normally store this in hidden config file FYI
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -24,6 +26,26 @@ export function createPost(props) {
 
   return {
     type: CREATE_POST,
+    payload: request,
+  };
+}
+
+// action creator to fetch a post
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request,
+  };
+}
+
+// action creator to delete a post
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request,
   };
 }
