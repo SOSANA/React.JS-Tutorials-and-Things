9b5c9bd597d94175cba0c364a2651f72d59d5d20
SOSANA <thesosana@gmail.com>
2016-07-06 13:50:54 -0600

    adding new route for ShowPost params

--- modernReactRedux/blog/src/routes.js
+++ modernReactRedux/blog/src/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/postsIndex';
 import NewPost from './components/newPost';
+import ShowPost from './components/showPost';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={NewPost} />
+    <Route path="posts/:id" component={ShowPost} />
   </Route>
 );
