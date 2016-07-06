522f5ff414f2ee767842a0991b286c63f050ff03
SOSANA <thesosana@gmail.com>
2016-07-06 16:27:01 -0600

    settting up basic auth example to show HOC

---
+++ modernReactRedux/hoc/src/components/header.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions'; // pulling in our .authenticate() action creator
+
+class Header extends Component {
+  authButton() {
+    if (this.props.authenticated) {
+      return <button onClick={() => this.props.authenticate(false)}>Sign Out</button>;
+    }
+
+    return <button onClick={() => this.props.authenticate(true)}>Sign in</button>;
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li className="nav-item">
+              {this.authButton()}
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    authenticated: state.authenticated,
+  };
+}
+
+// pulling in our .authenticate() action creator
+export default connect(mapStateToProps, actions)(Header);
---
+++ modernReactRedux/hoc/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup Pepper</li>
+        <li>1 Cup Salt</li>
+      </ul>
+    </div>
+  );
+};
