c44767f7d429e3e01d8d63d3cb31dd6a8ec0c7d9
SOSANA <thesosana@gmail.com>
2016-07-06 14:00:41 -0600

    add link to each post in list, wire up mapStateToProps and fetchPosts

--- modernReactRedux/blog/src/components/postsIndex.js
+++ modernReactRedux/blog/src/components/postsIndex.js
@@ -14,6 +14,19 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={'posts/' + post.id}>
+            <span className="pull-xs-right">{post.categories}</span>
+            <strong>{post.title}</strong>
+          </Link>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
@@ -22,10 +35,17 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        <h1>List of blog posts...</h1>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
 }
 
 /*
@@ -38,4 +58,4 @@ export default connect(null, mapDispatchToProps)(PostsIndex);
 */
 
 // avoiding above boilerplate
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
