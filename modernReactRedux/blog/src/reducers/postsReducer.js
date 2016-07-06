706a0a61437312c5cd6e3288b94f2e9e86eb862c
SOSANA <thesosana@gmail.com>
2016-07-06 14:01:51 -0600

    added new action.type FETCH_POST

--- modernReactRedux/blog/src/reducers/postsReducer.js
+++ modernReactRedux/blog/src/reducers/postsReducer.js
@@ -1,10 +1,12 @@
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 // two pieces of application state: all posts, and active posts
 const INITIAL_STATE = { all: [], post: null };
 
 export default function (state = INITIAL_STATE, action) {
   switch (action.type) {
+    case FETCH_POST:
+      return { ...state, post: action.payload.data };
     case FETCH_POSTS:
       // all: action.payload.data is our response from the api
       return { ...state, all: action.payload.data };
