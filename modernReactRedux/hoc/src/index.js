f7609b8286fe36e9ba5743b749e51edd48158b5a
SOSANA <thesosana@gmail.com>
2016-07-07 17:06:32 -0600

    adding auth HOC to our routes

--- modernReactRedux/hoc/src/index.js
+++ modernReactRedux/hoc/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router';
 
+import requireAuth from './components/requireAuth';
 import App from './components/app';
 import Resources from './components/resources';
 import reducers from './reducers';
@@ -14,7 +15,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="resources" component={Resources} />
+        <Route path="resources" component={requireAuth(Resources)} />
       </Route>
     </Router>
   </Provider>
