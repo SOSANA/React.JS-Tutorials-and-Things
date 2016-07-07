8a112688571f3afe983c78b4de131da24149359f
SOSANA <thesosana@gmail.com>
2016-07-07 16:27:18 -0600

    adding HOC for authentication

--- modernReactRedux/hoc/src/components/requireAuth.js
+++ modernReactRedux/hoc/src/components/requireAuth.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
+// purpose is to render a component that we pass to this function
 export default function (ComposedComponent) {
   class Authentication extends Component { //eslint-disable-line
+    // the purpose of '...this.props' is to satisfy and props being passed down
+    // in the component
     render() {
       return (
         <ComposedComponent {...this.props} />
@@ -11,3 +14,10 @@ export default function (ComposedComponent) {
 
   return Authentication;
 }
+
+// example of HOC
+// in some other location (not in this file) we want to use this HOC
+// import Authentication // this is my HOC
+// import Resources // this is the component I want to wrap
+// const ComposedComponent = Authentication(Resources);
+// <ComposedComponent /> // in some other render method
