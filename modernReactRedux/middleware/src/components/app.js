d26ed0e76aaaf28f98b892431efcf26d0987ff82
SOSANA <thesosana@gmail.com>
2016-07-08 17:38:51 -0600

    adding our new userlist component to app root

--- modernReactRedux/middleware/src/components/app.js
+++ modernReactRedux/middleware/src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import UserList from './userListContainer';
 
-export default class App extends Component {
+export default class App extends Component { // eslint-disable-line
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <UserList />
+      </div>
     );
   }
 }
