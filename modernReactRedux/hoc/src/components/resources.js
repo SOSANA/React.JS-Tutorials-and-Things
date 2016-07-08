7f6d0cadbcd53935d101a0338885017769311160
SOSANA <thesosana@gmail.com>
2016-07-08 15:39:42 -0600

    adding notes on an alternative to adding auth to every instance of a component

--- modernReactRedux/hoc/src/components/resources.js
+++ modernReactRedux/hoc/src/components/resources.js
@@ -12,3 +12,23 @@ export default () => {
     </div>
   );
 };
+
+
+/**
+ * if we wanted every instance of resources to be authenticated, here is an alternative
+ * and we would remove the requireAuth in our routes.js or src/index
+   const Resources = () => {
+   return (
+     <div>
+       Super Special Secret Recipe
+       <ul>
+         <li>1 Cup Sugar</li>
+         <li>1 Cup Pepper</li>
+         <li>1 Cup Salt</li>
+       </ul>
+     </div>
+   );
+ };
+
+ export default requireAuth(Resources);
+ */
