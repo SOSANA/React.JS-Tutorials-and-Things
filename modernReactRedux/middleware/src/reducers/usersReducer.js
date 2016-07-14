3a5b7616ccc256a1800b07dac6e8f1b5bd7cdb8a
SOSANA <thesosana@gmail.com>
2016-07-14 17:29:02 -0600

    applying our data array onto our action.payload

--- modernReactRedux/middleware/src/reducers/usersReducer.js
+++ modernReactRedux/middleware/src/reducers/usersReducer.js
@@ -3,8 +3,10 @@ import { FETCH_USERS } from '../actions/constants';
 export default function (state = [], action) {
   switch (action.type) {
     case FETCH_USERS:
+      // shows are action.payload object with data array
+      // console.log(action.payload);
       // spread operator takes existing state and concats on our new list of users
-      return [...state, ...action.payload];
+      return [...state, ...action.payload.data];
     default:
       return state;
   }
