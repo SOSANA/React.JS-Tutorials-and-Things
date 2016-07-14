305cb2c68292627f6764f1d8663d8fd2c8a67432
SOSANA <thesosana@gmail.com>
2016-07-14 17:27:19 -0600

    updating renderUser method user info

--- modernReactRedux/middleware/src/components/userListContainer.js
+++ modernReactRedux/middleware/src/components/userListContainer.js
@@ -11,8 +11,8 @@ class UserList extends Component {
     return (
       <div className="card card-block" key={user.name}>
         <h4 className="card-title">{user.name}</h4>
-        <p className="card-text">{user.work}</p>
-        <a href="/" className="btn btn-primary">Email</a>
+        <p className="card-text">{user.company.name}</p>
+        <a className="btn btn-primary" href={user.website}>Website</a>
       </div>
     );
   }
