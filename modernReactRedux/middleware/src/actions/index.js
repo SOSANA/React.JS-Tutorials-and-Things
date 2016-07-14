d6c5d3c2c3d812474b5c89e6a95dd6fdf2d596ee
SOSANA <thesosana@gmail.com>
2016-07-14 16:02:47 -0600

    wiring up axios get request promise

--- modernReactRedux/middleware/src/actions/index.js
+++ modernReactRedux/middleware/src/actions/index.js
@@ -1,22 +1,15 @@
+import axios from 'axios';
+
 import { FETCH_USERS } from './constants';
 
 export function fetchUsers() {
+  // Fake Online REST API for Testing and Prototyping
+  // src: http://jsonplaceholder.typicode.com/
+  // axios returning a promise
+  const request = axios.get('http://jsonplaceholder.typicode.com/users');
+
   return {
     type: FETCH_USERS,
-    // hard coding user data
-    payload: [
-      {
-        name: 'Jane',
-        work: 'Cheese Factory',
-      },
-      {
-        name: 'Alex',
-        work: 'ACME Anvil',
-      },
-      {
-        name: 'Jim',
-        work: 'Road Warrior Construction',
-      },
-    ],
+    payload: request,
   };
 }
