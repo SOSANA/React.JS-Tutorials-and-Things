37ca93dcbd7fb6395adfc4d36552fe5fc77477aa
SOSANA <thesosana@gmail.com>
2016-07-26 16:26:53 -0600

    adding new get handler route for any requests coming in that must pass our require auth step

--- modernReactRedux/authServer/server/routes/user.js
+++ modernReactRedux/authServer/server/routes/user.js
@@ -1,8 +1,18 @@
 import { Router } from 'express';
+import passport from 'passport';
 import * as Authentication from '../controllers/authentication';
-const router = new Router();
+import passportService from '../services/passport';
 
-// definging a route the user can visit
+const router = new Router();
+// any requests coming in, must pass this requireAuth step
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+// definging a route handler for a get request for our root route
+router.route('/').get(requireAuth, (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+// definging a route handler for a post request for our signup route
 router.route('/signup').post(Authentication.signup);
 
 export default router;
