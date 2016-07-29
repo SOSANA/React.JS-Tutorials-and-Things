06bf02a891c4f9c5c8c62ba1fa35817d8b508068
SOSANA <thesosana@gmail.com>
2016-07-29 14:05:20 -0600

    updating env port

--- modernReactRedux/authClient/server/server/config/serverConfig.js
+++ modernReactRedux/authClient/server/server/config/serverConfig.js
@@ -1,6 +1,6 @@
 const serverConfig = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/auth',
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 8089,
 };
 
 export default serverConfig;
