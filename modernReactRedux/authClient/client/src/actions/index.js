552d86181704cee911908761c486faef858a853f
SOSANA <thesosana@gmail.com>
2016-08-03 18:32:23 -0600

    updating auth actions, reducers, and changing naming of constants to types

--- modernReactRedux/authClient/client/src/actions/index.js
+++ modernReactRedux/authClient/client/src/actions/index.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import { browserHistorhy } from 'react-router';
-import { AUTH_USER } from './constants';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
 const ROOT_URL = 'http://localhost:8089';
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error,
+  };
+}
 
 export function signinUser({ email, password }) {
   return (dispatch) => {
@@ -12,14 +19,16 @@ export function signinUser({ email, password }) {
       // if request is good...
       // update to state to indicate user is authenticated
       dispatch({ type: AUTH_USER });
-      // save the JWT token
+      // save the JWT token to localStorage (which is a method available via the window scope)
+      localStorage.setItem('token', res.data.token);
       // redirect to the route '/feature'
       browserHistorhy.push('/feature');
     })
     .catch(() => {
-      // if request is bad...ls
+      // if request is bad...
 
       // show an error to the user
+      dispatch(authError('Bad Login Info'));
     });
   };
 }
