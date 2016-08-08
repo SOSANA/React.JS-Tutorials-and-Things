22b4eab02981324d0642ab5a9292a0d3784d5b56
SOSANA <thesosana@gmail.com>
2016-08-08 15:00:32 -0600

    adding our feature component that requires auth to be viewed

---
+++ modernReactRedux/authClient/client/src/components/feature.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feature extends Component { // eslint-disable-line
+  render() {
+    return (
+      <div>
+        This is a Feature
+      </div>
+    );
+  }
+}
+
+export default Feature;
