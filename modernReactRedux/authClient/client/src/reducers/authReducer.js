d7aac0d12f3d8dd8fe65084cf59bf7f40453d89d
SOSANA <thesosana@gmail.com>
2016-08-08 16:11:31 -0600

    updating reducer with action type for server side messages

--- modernReactRedux/authClient/client/src/reducers/authReducer.js
+++ modernReactRedux/authClient/client/src/reducers/authReducer.js
@@ -2,6 +2,7 @@ import {
   AUTH_USER,
   UNAUTH_USER,
   AUTH_ERROR,
+  FETCH_MESSAGE,
 } from '../actions/types';
 
 export default function (state = {}, action) {
@@ -14,6 +15,9 @@ export default function (state = {}, action) {
 
     case AUTH_ERROR:
       return { ...state, error: action.payload };
+
+    case FETCH_MESSAGE:
+      return { ...state, message: action.payload };
 
     default:
       return state;
