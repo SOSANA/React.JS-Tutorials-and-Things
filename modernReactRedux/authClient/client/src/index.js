0bab95d1cd9d6b8f2a1f3caa6954cd2ee9ab6d51
SOSANA <thesosana@gmail.com>
2016-08-08 16:14:14 -0600

    wiring up persistant token to fix bug when refreshing the page and loosing token

--- modernReactRedux/authClient/client/src/index.js
+++ modernReactRedux/authClient/client/src/index.js
@@ -21,7 +21,7 @@ const token = localStorage.getItem('token');
 
 // if we have a token, consider the user to be signed in
 if (token) {
-  // we need to update applicaiton state
+  // we need to update application state
   store.dispatch({ type: AUTH_USER });
 }
 
