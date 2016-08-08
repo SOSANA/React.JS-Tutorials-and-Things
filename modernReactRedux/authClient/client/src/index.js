6e8f7c09b7ae6c1ccf96c509ff1d09ad10647c24
SOSANA <thesosana@gmail.com>
2016-08-08 14:59:30 -0600

    wiring up routes for feature, require auth, and welcome

--- modernReactRedux/authClient/client/src/index.js
+++ modernReactRedux/authClient/client/src/index.js
@@ -9,7 +9,10 @@ import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import Feature from './components/feature';
+import requireAuth from './components/auth/requireAuth';
 import reducers from './reducers';
+import Welcome from './components/welcome';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
@@ -17,9 +20,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
+        <Route path="feature" component={requireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>
