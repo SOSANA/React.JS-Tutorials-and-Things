33c83dcc7d8599405e20d24d7a23fbece0a95e6a
SOSANA <thesosana@gmail.com>
2016-08-12 21:45:21 -0600

    removing query prop

--- modernReactRedux/authServer/webpack.config.dev.js
+++ modernReactRedux/authServer/webpack.config.dev.js
@@ -1,8 +1,9 @@
 import path from 'path';
 import webpack from 'webpack';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const webpackConfig = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval-cheap-module-source-map',
   entry: [
     'webpack-hot-middleware/client',
   './client/main'
@@ -10,48 +11,51 @@ const webpackConfig = {
   output: {
     path: path.join(__dirname, 'public', 'js'),
     filename: 'bundle.js',
-    publicPath: '/js'
+    publicPath: '/js/',
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin({ multistep: true }),
     new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin("css/[name].css?[hash]-[chunkhash]-[contenthash]-[name]", {
+      disable: false,
+      allChunks: true
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     })
   ],
   module: {
     loaders: [
       {
-        test: /\.css$/,
-        loader: 'style!css?modules',
-      },
+        test: /\.css$/, loader: ExtractTextPlugin.extract(
+				"style-loader",
+				"css-loader?sourceMap",
+				{
+					publicPath: "/css/"
+				}
+      )},
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          plugins: [
-            ['react-transform', {
-              transforms: [
-                {
-                  transform: 'react-transform-hmr',
-                  imports: ['react'],
-                  locals: ['module']
-                }, {
-                  transform: 'react-transform-catch-errors',
-                  imports: ['react', 'redbox-react']
-                }
-              ]
-            }]
-          ]
-        }
+        test: /\.js$/, // test for '.js' file, if it passes use the loader
+        exclude: /node_modules/, // don't include node modules
+        loader: 'babel', // short for 'babel-loader'
       }
     ]
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  );
+}
 
 export default webpackConfig;
