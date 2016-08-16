4d4e776378f77400422f6bc2f9680c9a85356a57
SOSANA <thesosana@gmail.com>
2016-08-16 16:51:35 -0600

    adding ROOT_URL API to our server config

--- modernReactRedux/authServer/server/config/serverConfig.js
+++ modernReactRedux/authServer/server/config/serverConfig.js
@@ -1,6 +1,7 @@
 const serverConfig = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/auth',
   port: process.env.PORT || 8080,
+  ROOT_URL: 'http://localhost:8080/api',
 };
 
 export default serverConfig;
