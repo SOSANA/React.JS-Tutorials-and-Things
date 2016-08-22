c1803c4b6e41eae2864eca371e74a5a5b27c78f1
SOSANA <thesosana@gmail.com>
2016-08-22 13:09:17 -0600

    updating notes on expect test matchers

--- modernReactRedux/testing/notes/notes.js
+++ modernReactRedux/testing/notes/notes.js
@@ -14,7 +14,19 @@
  *    - used to test a single attribute of a target.
  *    - 'it' blocks try to make an assertion about a very particular
  *      fact about the testing subject
+ *    - uses a 'matcher' that are chainable functions
+ *      - ex: https://github.com/chaijs/chai-jquery dsfafafadsfafds
+ *        - only used for tests
  *  - expect:
  *    - used to make an 'assertion' about a target
  *    - Used for a very particular attribute for our target
+ *
+ * Writing Spec's:
+ *  - we make assertion about the end product
+ *  - we should have a list of what we care about
+ *    - ex: a comment area
+ *      - we care about
+ *        - it has a text area
+ *        - it has a button
+ *        - entering text into the text area updates the text
  */
