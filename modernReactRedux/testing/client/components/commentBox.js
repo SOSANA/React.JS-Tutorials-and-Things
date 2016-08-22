eed202d0c0ed4394255b60c3698b91518174058d
SOSANA <thesosana@gmail.com>
2016-08-22 16:56:06 -0600

    updating with form element, new event handlers for handleChange and handleSubmit

--- modernReactRedux/testing/client/components/commentBox.js
+++ modernReactRedux/testing/client/components/commentBox.js
@@ -1,1 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+export default class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { comment: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ comment: e.target.value });
+  }
+
+  handleSubmit(e) {
+    // keep form from trying to submit itself
+    e.preventDefault();
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="comment-box">
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange}
+        />
+        <button action="submit">Submit Comment</button>
+      </form>
+    );
+  }
+}
