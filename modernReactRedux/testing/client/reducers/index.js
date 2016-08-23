2a43b81d928d1a8f5f06a6365d1887392f470997
SOSANA <thesosana@gmail.com>
2016-08-22 18:46:13 -0600

    updating inital state for comments

--- modernReactRedux/testing/client/reducers/index.js
+++ modernReactRedux/testing/client/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  comments: (state = []) => state,
 });
 
 export default rootReducer;
