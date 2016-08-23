f50b87177efe42bec87062429a379aac89f9cffe
SOSANA <thesosana@gmail.com>
2016-08-22 18:47:30 -0600

    updating comment list to test for assertions on showing an li element for each comment, and showing each comment that is provided

--- modernReactRedux/testing/test/components/commentList.test.js
+++ modernReactRedux/testing/test/components/commentList.test.js
@@ -1,6 +1,27 @@
+/**
+ * what we care about:
+ *  - it shows an "li" element for each comment
+ *  - it shows each comment that is provided
+ */
+
 import { renderComponent, expect } from '../test.helper';
-import CommentBox from '../../client/components/commentList';
+import CommentList from '../../client/components/commentList';
 
-describe('CommentBox', () => {
+describe('CommentList', () => {
+  let component;
 
+  beforeEach(() => {
+    // stubbing in some dummy text
+    const props = { comments: ['new comment', 'other new comment'] };
+    component = renderComponent(CommentList, null, props);
+  });
+
+  it('shows an "li" element for each comment', () => {
+    return expect(component.find('li').length).to.equal(2);
+  });
+
+  it('shows each comment that is provided', () => {
+    expect(component).to.contain('new comment');
+    expect(component).to.contain('other new comment');
+  });
 });
