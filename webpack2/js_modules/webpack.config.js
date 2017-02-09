400dce52cbe2cb72c1e54ea6ef0d778fe373b803
sosana <thesosana@gmail.com>
2017-02-08 17:06:30 -0800

    wiring up webpack extract-text-webpack plugin for seperate css bundling

--- webpack2/js_modules/webpack.config.js
+++ webpack2/js_modules/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); // plugin for css bundles
 
 const config = {
   // entry property kick starts the entry point of the app
@@ -15,11 +16,21 @@ const config = {
         test: /\.js$/ // reg ex looks for .js files when importing in each file
       },
       {
-        use: ['style-loader', 'css-loader'], // order important, loaded right to left
+        // order important, loaded right to left, loads script for you in header but bundles it all into
+        // one file with bundle.js rather than seperate file
+        // use: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
         test: /\.css$/ // reg ex looks for .css files when importing in each file
       }
     ]
-  }
+  },
+  plugins: [
+    // tells ExtractTextPlugin to find any files with .css, if we have multiple .css files it will be
+    // stuffed into this one file
+    new ExtractTextPlugin('style.css')
+  ]
 }
 
 module.exports = config;
