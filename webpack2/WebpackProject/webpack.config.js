920e852dff8d1de9522a9289c929ee18e250ac8a
sosana <thesosana@gmail.com>
2017-02-17 14:31:09 -0800

    adding html-webpack-plugin

--- webpack2/WebpackProject/webpack.config.js
+++ webpack2/WebpackProject/webpack.config.js
@@ -1,11 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// grabbing all project dependencies
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+  'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  // passing object for multiple entry points
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js' // grabs entry.bundle name key ex: 'bundle.js'
   },
   module: {
     rules: [
@@ -19,5 +30,15 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    // this helps solve double including VENDOR_LIBS
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    // this helps manage script tags for html and manually adds a script tag
+    new HtmlWebpackPlugin({
+      template: 'src/index.html' // location of html
+    })
+  ]
 };
