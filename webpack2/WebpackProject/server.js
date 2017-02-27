b78748a898fc76dc71d271b8b9b80c8ff71bb1c5
sosana <thesosana@gmail.com>
2017-02-27 14:43:05 -0800

    adding console logs to help with dev/prod env

--- webpack2/WebpackProject/server.js
+++ webpack2/WebpackProject/server.js
@@ -7,11 +7,13 @@ const app = express();
 app.get('/hello', (req, res) => res.send({ hi: 'there' }));
 
 if (process.env.NODE_ENV !== 'production') {
+  console.log('hello I\'m in development');
   const webpackMiddleware = require('webpack-dev-middleware');
   const webpack = require('webpack');
   const webpackConfig = require('./webpack.config.js');
   app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
+  console.log('hello I\'m in production');
   app.use(express.static('dist'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
