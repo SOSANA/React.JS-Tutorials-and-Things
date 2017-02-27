19aeeeb4f20ef02740e7562b9d575eb7a03c8c1f
sosana <thesosana@gmail.com>
2017-02-27 12:40:35 -0800

    updating project for code splitting, import not working using System.import()

--- webpack2/WebpackProject/src/router.js
+++ webpack2/WebpackProject/src/router.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Router, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
+
 // no need for this imports as used in componentRoutes object
 // import ArtistDetail from './components/artists/ArtistDetail';
 // import ArtistCreate from './components/artists/ArtistCreate';
@@ -11,33 +12,31 @@ import ArtistMain from './components/artists/ArtistMain';
 const componentRoutes = {
   component: Home,
   path: '/',
-  IndexRoute: { component: ArtistMain },
+  indexRoute: { component: ArtistMain },
   childRoutes: [
     {
       path: 'artists/new',
       getComponent(location, cb) { // async load functions
-        import('./components/artists/ArtistCreate') // path to module component
+        System.import('./components/artists/ArtistCreate') // path to module component
           .then(module => cb(null, module.default));
       }
     },
     {
       path: 'artists/:id',
       getComponent(location, cb) {
-
-        import('./components/artists/ArtistDetail') // path to module component
+        System.import('./components/artists/ArtistDetail')
           .then(module => cb(null, module.default));
       }
     },
     {
       path: 'artists/:id/edit',
       getComponent(location, cb) {
-
-        import('./components/artists/ArtistEdit') // path to module component
+        System.import('./components/artists/ArtistEdit')
           .then(module => cb(null, module.default));
       }
     }
   ]
-}
+};
 
 const Routes = () => {
   return (
@@ -46,3 +45,4 @@ const Routes = () => {
 };
 
 export default Routes;
+
